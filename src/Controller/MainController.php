ea7ffba0e5f2e2080041062f5b08de329192777d
winlex <alexandergurin1996@gmail.com>
2021-08-25 19:51:05 +0300

    Добваил создание файла

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,16 +3,35 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\FilesRepository;
+use App\Repository\UsersRepository;
+use App\Entity\Files;
+use App\Entity\Users;
+use DateTimeInterface;
 
 class MainController extends AbstractController
 {
-    #[Route('/', name: 'index')]
-    public function index(): Response
+    #[Route('/file/create', name: 'create', methods: 'post')]
+    public function create(Request $request): Response
     {
-        return $this->render('main/index.html.twig', [
-            'controller_name' => 'MainController',
-        ]);
+        $entityManager = $this->getDoctrine()->getManager();
+        date_default_timezone_set('Europe/Moscow');
+        $file = new Files();
+
+        $file->setName($request->request->get('name'));
+        $file->setContent($request->request->get('content'));
+        $file->setType(0); //0 - file
+        $file->setStatus(0);
+        $file->setDateCreate(new \DateTime());
+        $file->setDateModify(new \DateTime());
+        $file->setPath($request->request->get('path'));
+
+        $entityManager->persist($file);
+        $entityManager->flush();
+
+        return new Response('Saved new product with id '.$file->getId());
     }
 }
